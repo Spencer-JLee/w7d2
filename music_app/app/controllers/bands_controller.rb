14fa808aa1264407cb819c84e61394659d18bb34
App Academy Student <email@email.com>
2022-03-15 14:19:58 -0400

    added views for bands

---
+++ music_app/app/controllers/bands_controller.rb
@@ -0,0 +1,61 @@
+class BandsController < ApplicationController
+  def index
+    @bands = Band.all
+    render :index
+  end
+
+  def show
+    @band = Band.find(params[:id])
+
+    if @band
+      render :show
+    else
+      render :index
+    end
+  end
+
+  def new
+    @band = Band.new
+
+    render :new
+  end
+
+  def create
+    @band = Band.new(bands_params)
+
+    if @band.save!
+      redirect_to band_url(@band)
+    else
+      render :new
+    end
+
+  end
+
+  def edit
+    @band = Band.find(params[:id])
+
+    render :edit
+  end
+
+  def update
+    @band = Band.find(params[:id])
+
+    if @band.update(bands_params)
+      redirect_to band_url(@band)
+    else
+      redirect_to :index
+    end
+  end
+
+  def destroy
+    @band = Band.find(params[:id])
+    @band.destroy
+    redirect_to :index
+  end
+
+
+  private
+  def bands_params
+    params.require(:band).permit(:name)
+  end
+end
--- music_app/app/models/user.rb
+++ music_app/app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   after_initialize :ensure_session_token
   validates :email, :session_token, presence: true, uniqueness: true
   validates :password_digest, presence: true
+  validates :password, length: {minimum: 6, allow_nil: true}
 
   def self.generate_session_token
     SecureRandom::urlsafe_base64(16)

82d673773aaa8577615774c6f3d6dbb973e9ea4b
App Academy Student <email@email.com>
2022-03-15 11:57:32 -0400

    worked on session and user controllers

---
+++ music_app/app/controllers/users_controller.rb
@@ -0,0 +1,36 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+    render :index
+  end
+
+  def show
+    @user = User.find(params[:id])
+
+    if @user
+      render :show
+    else
+      render :index
+    end
+  end
+
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def create
+    @user = User.create(user_params)
+    if @user.save
+      login(@user)
+      redirect_to user_url(@user)
+    else
+      
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+end
---
+++ music_app/db/migrate/20220315152242_create_users.rb
@@ -0,0 +1,11 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :email, null: false, unique: true
+      t.text :password_digest, null: false
+      t.text :session_token, null: false, unique: true
+    end
+    add_index :users, :email
+    add_index :users, :session_token
+  end
+end

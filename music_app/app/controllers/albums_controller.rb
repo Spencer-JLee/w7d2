e2791c84fa82d2e887a4db0efd8e0c1f1f302d94
App Academy Student <email@email.com>
2022-03-15 15:06:41 -0400

    added controller methods

---
+++ music_app/app/controllers/albums_controller.rb
@@ -0,0 +1,53 @@
+class AlbumsController < ApplicationController
+  def show
+    @album = Album.find(params[:id])
+
+    if @album
+      render :show
+    else
+      redirect_to :new
+    end
+  end
+  
+  def new
+    @album = Album.new
+    render :new
+  end
+
+  def create
+    @album = Album.new(albums_params)
+
+    if @album.save!
+      redirect_to album_url(@album)
+    else
+      redirect_to :new
+    end
+  end
+
+  def edit
+    @album = Album.find(params[:id])
+
+    render :edit
+  end
+
+  def update
+    @album = Album.find(params[:id])
+
+    if @album.update(albums_params)
+      redirect_to album_url(@album)
+    else
+      redirect_to :new
+    end
+  end
+
+  def destroy
+    @album = Album.find(params[:id])
+    @album.destroy
+    redirect_to :new
+  end
+
+  private
+  def albums_params
+    params.require(:album).permit(:title, :year, :is_live, :band_id)
+  end
+end
